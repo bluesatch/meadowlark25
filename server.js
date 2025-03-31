c137410f5e7049c8a1c753ce16766012c14f6a46
bluesatch <satchwerk@gmail.com>
2025-03-31 14:21:55 -0500

    Moved 'fortune' into module

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
 const express = require('express')
+// for handlebars
+const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
 
 const app = express()
 
 const port = process.env.port || 3000
 
 
-// home page
-app.get('/', (req, res)=> {
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
 
+// add public as static directory
+app.use(express.static(__dirname + '/public'))
+
+//configure view engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main'
+}))
+
+app.set('view engine', 'handlebars')
+
+
+// home page
+// .get(path, callback func)
+app.get('/', (req, res)=> res.render('home'))
+
+// about page
 app.get('/about', (req, res)=> {
-    res.type('text/plaiin')
-    res.send('About Meadowlark Travel')
+    res.render('about', { fortune: fortune.getFortune()})
 })
 
 // custom 404 page
 app.use((req, res)=> {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - Not Found')
+    res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next)=> {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Server Error')
+    res.render('500')
 })
 
 app.listen(port, ()=> console.log(
